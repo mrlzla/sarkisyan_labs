e1046723696a8c2212e94eacf2b9f998ea29222c
Vladislav Artemyev <mrlzlamogilev@gmail.com>
2018-04-05 14:06:00 +0300

    Update circle.h

--- lab1_2/circle.h
+++ lab1_2/circle.h
@@ -9,6 +9,7 @@ class Circle:IElement
 public:
     Circle(cv::Point center, unsigned int radius);
     void Draw(cv::Mat & canvas, cv::Scalar & color);
+    void Accept(Visitor &v);
 private:
     cv::Point center;
     unsigned int radius;
