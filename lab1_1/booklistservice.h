e93887fa3c13cd1e2a7a3e374c3386b614bdf5e6
Vladislav Artemyev <mrlzlamogilev@gmail.com>
2018-04-05 13:57:20 +0300

    Update booklistservice.h

--- lab1_1/booklistservice.h
+++ lab1_1/booklistservice.h
@@ -3,20 +3,20 @@
 
 #include "basictypes.h"
 #include "book.h"
-#include "repository.h"
+#include "irepository.h"
 
 
 class BookListService
 {
 public:
-    BookListService();
+    BookListService(IRepository<BookPtr> book_repository);
     void AddBook(const BookPtr item);
     void RemoveBook(const BookPtr & item);
     BookPtr FindBookById(const unsigned int id);
     void SortBooksById();
 
 private:
-    std::shared_ptr< Repository<BookPtr> > book_repository;
+    std::shared_ptr< IRepository<BookPtr> > book_repository;
 };
 
 #endif // BOOKLISTSERVICE_H
